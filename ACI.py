2d4c47da3dc45a394feb8ba15053298470c911df
Goswami <RG090077@cerner.net>
2023-03-15 15:32:21 +0530

    Hop Trace Phase 1 - Identify endpoint in ACI across DCs

---
+++ ACI.py
@@ -0,0 +1,62 @@
+"""This python file contains ACI API methods related to different functionalities.
+It is created to be used as it is in different Projects as per your requirement"""
+
+# import necessary modules
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+# Create class for ACI
+
+
+class CiscoAPIC:
+    # Assign attributes corresponding to APICs
+    def __init__(self, dcapic):
+        self.apic = dcapic
+
+# Create a method for login to APIC using API
+    def login(self, usr, passwd):
+        """Method to Login to user provided DC APIC
+
+        Returns : [String] Cookie to be used in subsequent API calls to this DC APIC"""
+        login_url = 'https://'+self.apic+'/api/aaaLogin.json'
+        login_request_body = {
+            "aaaUser": {
+                "attributes": {
+                    "name": usr,
+                    "pwd": passwd
+                }
+            }
+        }
+        # Create a POST request to push Login credentials to APIC, use verify=False if APIC does not use SSL
+        print("Logging in to "+self.apic)
+        try:
+            resp = requests.post(url=login_url, json=login_request_body, verify=False)
+            resp_output = resp.json()
+            # Check if user creds allowed login by TACACS auth of ACI
+            if resp_output['imdata'][0] == 'error':
+                if resp_output['imdata'][0]['error']['attributes']['code'] == '401':
+                    print(resp_output['imdata'][0]['error']['attributes']['text'])
+                    raise SystemExit('Exiting due to incorrect Username/Password...')
+            else:
+                cookie = {'APIC-Cookie': resp_output['imdata'][0]['aaaLogin']['attributes']['token']}
+                print("Successfully logged in for "+usr+"...")
+                return cookie
+        except requests.exceptions.RequestException as error:
+            raise SystemExit(error)
+
+    def searchendpointip(self, ipaddress, session_cookie):
+        """Method to search of endpoint using IP Address in APIC
+
+        Returns: [Dictionary] Details of Endpoint IP"""
+        print("Searching for Endpoint with IP "+ipaddress+" in "+self.apic+"...")
+        search_url = 'https://'+self.apic + '/api/node/class/fvCEp.json?rsp-subtree=full&rsp-subtree-include=required&rsp-subtree-filter=eq(fvIp.addr,"'+ipaddress+'")'
+        try:
+            resp = requests.get(url=search_url, cookies=session_cookie, verify=False)
+            resp_output = resp.json()
+            resp.close()
+            return resp_output
+        except requests.exceptions.RequestException as error:
+            raise SystemExit(error)
---
+++ main.py
@@ -0,0 +1,61 @@
+from ACI import CiscoAPIC
+import getpass
+import os
+
+
+def getcredentials():
+    """Method to obtain Cerner ASP credential from user
+
+    Returns: [String, String]: username and password"""
+    username = input("Please enter your CERNER ASP username: ")
+    password = getpass.getpass("Please enter your CERNER ASP password: ")
+    return username, password
+
+
+class Endpoint:
+    """Class to work on endpoint"""
+    def __int__(self):
+        self.ipaddress = None
+        self.macaddress = None
+        self.tenant = None
+        self.epg = None
+        self.switch = None
+        self.interface = None
+
+    def getipaddress(self):
+        """Method to obtain IP address to be searched from user"""
+        self.ipaddress = input("Please enter the IP address to search for: ")
+
+    def searchendpointinaci(self):
+        """Method to search user provided IP in all ACI Fabrics across KC/LS RHO"""
+        usr, passwd = getcredentials()
+        dclist = ["ls2apic3", "ls3apic3", "ls4apic3", "ls5apic3", "ls6apic3", "kc1apic3", "kc2apic3", "kc3apic1", "kc8apic1"]
+        for apic in dclist:
+            dcapic = CiscoAPIC(apic)
+            session_cookie = dcapic.login(usr, passwd)
+            epdetails = dcapic.searchendpointip(self.ipaddress, session_cookie)
+            if epdetails['totalCount'] == '0':
+                print("Could not find endpoint in "+apic+"...\n")
+                continue
+            else:
+                print('Found details of Endpoint as below: ')
+                self.macaddress = epdetails['imdata'][0]["fvCEp"]["attributes"]["mac"]
+                self.tenant = epdetails['imdata'][0]['fvCEp']['attributes']['dn']
+                self.epg = epdetails['imdata'][0]['fvCEp']['attributes']['dn']
+                self.switch = epdetails['imdata'][0]['fvCEp']['children'][0]['fvIp']['children'][0]['fvReportingNode']['attributes']['id']
+                print(f'IP: {self.ipaddress}')
+                print(f'MAC: {self.macaddress}')
+                self.tenant = self.tenant.split('/')[1].split('-')[1]  # original output = "uni/tn-uhbny/ap-rhoAppProfile/epg-159_140_33_128/cep-02:50:56:87:46:FB"
+                print(f'Tenant: {self.tenant}')
+                self.epg = self.epg.split('/')[3].split('-')[1]  # original output = "uni/tn-uhbny/ap-rhoAppProfile/epg-159_140_33_128/cep-02:50:56:87:46:FB"
+                print(f'EPG: {self.epg}')
+                print(f'Switch: {self.switch}')
+                break
+
+
+if __name__ == '__main__':
+    os.system('cls')
+    print("Welcome to Endpoint Search Tool!")
+    ep = Endpoint()
+    ep.getipaddress()
+    ep.searchendpointinaci()
